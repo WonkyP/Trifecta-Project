b6454ad75aa3d1b17e0f7cfaad1f5fdf8860807c
carl69 <carlmorgenstierne@yahoo.com>
2018-09-05 17:31:08 +0200

    Started on a scene entry script and making world codes to enter other scenes

---
+++ Assets/Scripts/WorldCode/WC_EnteringBossPuzzle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WC_EnteringBossPuzzle : MonoBehaviour {
+
+    public int sceneIndexNumber; // we have to set the scene index our self :(
+    bool entryGrantet = false;
+
+	
+	void Update () {
+        if (entryGrantet)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                print("YOU MAY PASS");
+                SceneManager.LoadScene(sceneIndexNumber);
+            }
+            
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10) // checks what layer the triggering object is and activates if it's the "Player" layer
+        {
+            entryGrantet = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 10) // checks what layer the triggering object is and deactivates if it's the "Player" layer
+        {
+            entryGrantet = false;
+        }
+    }
+
+}
+
