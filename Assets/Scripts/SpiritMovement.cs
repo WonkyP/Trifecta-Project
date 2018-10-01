6a3f07c8109aec3eb93dc8a5eb6c1a0fde010e31
carl69 <carlmorgenstierne@yahoo.com>
2018-10-01 16:33:54 +0200

    Fixing the level for the build

--- Assets/Scripts/SpiritMovement.cs
+++ Assets/Scripts/SpiritMovement.cs
@@ -118,8 +118,8 @@ public class SpiritMovement : MonoBehaviour
 
 
         // Debug Raycast
-        Debug.DrawRay(transform.position + new Vector3(transform.lossyScale.x / 2 + 0.4f, 0.0f, 0.0f), Vector2.right * transform.localScale.x, Color.green);
-        Debug.DrawRay(transform.position - new Vector3(transform.lossyScale.x / 2 + 0.4f, 0.0f, 0.0f), Vector2.left * transform.localScale.x, Color.red);
+        Debug.DrawRay(transform.position + new Vector3(transform.lossyScale.x / 2 + 0.4f, 0.5f, 0.0f), Vector2.right * transform.localScale.x, Color.green);
+        Debug.DrawRay(transform.position - new Vector3(transform.lossyScale.x / 2 + 0.4f, -0.5f, 0.0f), Vector2.left * transform.localScale.x, Color.red);
 
     }
 
@@ -208,7 +208,7 @@ public class SpiritMovement : MonoBehaviour
         //if (script.right)
         rightHit = Physics2D.Raycast(transform.position + new Vector3(transform.lossyScale.x / 2 + 0.4f, 0.5f, 0.0f), Vector2.right * transform.localScale.x, 1.0f);
         //else
-        leftHit = Physics2D.Raycast(transform.position - new Vector3(transform.lossyScale.x / 2 + 0.4f, 0.5f, 0.0f), Vector2.left * transform.localScale.x, 1.0f);
+        leftHit = Physics2D.Raycast(transform.position - new Vector3(transform.lossyScale.x / 2 + 0.4f, -0.5f, 0.0f), Vector2.left * transform.localScale.x, 1.0f);
 
 
         if (rightHit.collider != null)
