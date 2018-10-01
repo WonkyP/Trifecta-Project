36dc8e87f12b82d32306873c42ab3245795afd0c
carl69 <carlmorgenstierne@yahoo.com>
2018-10-01 12:27:48 +0200

    The level is filled

---
+++ Assets/Scripts/camera/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float cSpeed;
+    public float cLength;
+
+    Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        transform.position = new Vector2 (player.position.x, player.position.y);
+	}
+}
