e5bf4e4faae1ebd5e6ab881a0577ecde960f19d7
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2018-10-03 17:46:00 +0200

    Added a preliminary dialogue script and trigger to project. It goes through the string array set in inspector on the trigger. Updated player prefab to accomodate for the floating text above the character.

---
+++ Assets/_Personal/Jon/Jon_Scripts/Trigger_Dialogue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigger_Dialogue : MonoBehaviour {
+
+    private GameObject player;
+    public string[] texts;
+    public Text floatingText;
+
+	// Use this for initialization
+	void Start () {
+        //Getting references
+        player = GameObject.FindGameObjectWithTag("Player");
+        floatingText = player.transform.GetChild(0).transform.GetChild(0).GetComponent<Text>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Starting coroutine on trigger enter.
+        if(collision.tag == "Player")
+        {
+            StartCoroutine("DisplayTexts");
+        }
+    }
+    
+    //Coroutine for displaying messages in the trigger's public strings one at a time with a delay.
+    IEnumerator DisplayTexts()
+    {
+        foreach(string message in texts)
+        {
+            floatingText.text = message;
+            yield return new WaitForSeconds(5);
+        }
+        
+        yield return null;
+        //Reset floating text over player and destroy trigger.
+        floatingText.text = "";
+        Destroy(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
