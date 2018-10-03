d0a60e6d8184355042cee0fd6aee785db0dcc5a2
carl69 <carlmorgenstierne@yahoo.com>
2018-10-03 15:47:16 +0200

    Moved my personal folder, and cleaned up the gamemanager so it don't need the canvas prefab anymore

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,7 @@ public class GameManager : MonoBehaviour {
 
     public static GameManager instance;
 
-    public Text abilitySelected;
-    public Text abilityExplanation;
 
-    public Image canvasCharacterImage;
-    public Image youWin;
-
-    private Animator canvasAnimator;
 
     // This manages the current character the player is using
     int currentCharacter = 0;
@@ -22,17 +16,17 @@ public class GameManager : MonoBehaviour {
     {
         instance = this;
 
-        canvasAnimator = canvasCharacterImage.GetComponent<Animator>();
-        //abilitySelected.text = "Choosen Character " + currentCharacter;
-        //abilityExplanation.text = "Daughter can Jump by pressing 'Z'"; Maybe for monday 17/09/18 will be useful to use this
-        abilitySelected.text = "";
-        abilityExplanation.text = "";
+        //canvasAnimator = canvasCharacterImage.GetComponent<Animator>();
+        ////abilitySelected.text = "Choosen Character " + currentCharacter;
+        ////abilityExplanation.text = "Daughter can Jump by pressing 'Z'"; Maybe for monday 17/09/18 will be useful to use this
+        //abilitySelected.text = "";
+        //abilityExplanation.text = "";
 
-        if (youWin != null)
-        {
-            youWin.enabled = false;
+        //if (youWin != null)
+        //{
+        //    youWin.enabled = false;
 
-        }
+        //}
     }
 
     // Update is called once per frame
@@ -42,59 +36,59 @@ public class GameManager : MonoBehaviour {
     }
     public void missionComplete()
     {
-        if (youWin != null)
-        {
-            youWin.enabled = true;
+        //if (youWin != null)
+        //{
+        //    youWin.enabled = true;
 
-        }
+        //}
     }
 
     public void updateHUD(int nr) //change the image showed on the HUD
     {
 
-        switch (nr)
-        {
-            case 0: // change to the girl
-                canvasAnimator.Play("HUDGirl");
-                break;
+        //switch (nr)
+        //{
+        //    case 0: // change to the girl
+        //        canvasAnimator.Play("HUDGirl");
+        //        break;
          
-            case 1: // change to the warrior 
-                canvasAnimator.Play("HUDWarrior");
-                break;
-            case 2://cahnge to the wizarld
-                canvasAnimator.Play("HUDwizard");
-                break;
+        //    case 1: // change to the warrior 
+        //        canvasAnimator.Play("HUDWarrior");
+        //        break;
+        //    case 2://cahnge to the wizarld
+        //        canvasAnimator.Play("HUDwizard");
+        //        break;
 
-        }
+        //}
 
     }
 
     void printCharacterInfo()
     {
-        string name;
-        string explanation;
+        //string name;
+        //string explanation;
 
-        switch (currentCharacter)
-        {
-            case 0:
-                name = "Daughter";
-                explanation = "Daughter can Jump by pressing 'Z'";
-                break;
-            case 1:
-                name = "Spirit of the forest";
-                explanation = "Spirit of the forest can move heavy blocks by pressing 'Z'";
-                break;
-            case 2:
-                name = "Father";
-                explanation = "Father can reveal the darkest secrets by pressing 'Z'";
-                break;
-            default:
-                name = "No character selected correctly";
-                explanation = "Daughter can Jump by pressing 'Z'";
-                break;
-        }
+        //switch (currentCharacter)
+        //{
+        //    case 0:
+        //        name = "Daughter";
+        //        explanation = "Daughter can Jump by pressing 'Z'";
+        //        break;
+        //    case 1:
+        //        name = "Spirit of the forest";
+        //        explanation = "Spirit of the forest can move heavy blocks by pressing 'Z'";
+        //        break;
+        //    case 2:
+        //        name = "Father";
+        //        explanation = "Father can reveal the darkest secrets by pressing 'Z'";
+        //        break;
+        //    default:
+        //        name = "No character selected correctly";
+        //        explanation = "Daughter can Jump by pressing 'Z'";
+        //        break;
+        //}
 
-        abilitySelected.text = "Choosen Character: " + name;
-        abilityExplanation.text = explanation;
+        //abilitySelected.text = "Choosen Character: " + name;
+        //abilityExplanation.text = explanation;
     }
 }
