4e6f1dbc81e840233e95e3a06124492be88ee5a1
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2018-10-03 15:12:03 +0200

    Finished a tutorial trigger with functionality as outlined in Trello. Needs updated artwork. Trigger prefab in prefabs/Triggers/. Changed name of Canvas_Menu_Jon to Canvas_Menu and updated prefab to to work with trigger. Added tag 'Canvas_Menu'.

--- Assets/_Personal/Jon/Jon_Scripts/Trigger_Tutorial.cs
+++ Assets/_Personal/Jon/Jon_Scripts/Trigger_Tutorial.cs
@@ -5,26 +5,43 @@ using UnityEngine.UI;
 
 public class Trigger_Tutorial : MonoBehaviour {
 
-    public bool isPaused;
-
     public Text tutText;
+    public string textToDisplay;
     private GameObject tutBox;
-
+    private Button tutBoxButton;
     // Use this for initialization
+
     void Start () {
-        tutBox = GameObject.FindGameObjectWithTag("Canvas").transform.GetChild(2).gameObject;
+        //Trigger is finding references in the canvas to store, then deactivating the tutorial box. There must be Canvas_Menu prefab in the scene hierarchy.
+        tutBox = GameObject.FindGameObjectWithTag("Canvas_Main").transform.GetChild(2).gameObject;
         tutText = tutBox.transform.GetChild(0).GetComponent<Text>();
+        tutBoxButton = tutBox.transform.GetChild(1).GetComponent<Button>();
         tutBox.SetActive(false);
 
     }
 
+    //When player enters trigger
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
-        {
+        {   
+            //Activates tutorial box and displays text as input from trigger gameObject in inspector. Stops time.
             tutBox.SetActive(true);
-            
+            tutText.text = textToDisplay;
+            //Adds button functionality to the button in the tutorial box.
+            tutBoxButton.onClick.AddListener(OKbutton);
+            Time.timeScale = 0;
         }
+    }
+
+    //Button function in tutorial box.
+    public void OKbutton()
+    {
+        //Deactivate tutorial box and resume time.
+        tutBox.SetActive(false);
+        Time.timeScale = 1;
+        //Destroy the trigger from the scene.
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
