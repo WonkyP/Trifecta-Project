6252b4edfbf78f3f5b6df439698ff27ad55f2f8a
Pablo <pablma11@ucm.es>
2018-10-03 13:26:59 +0200

    Platform movement refined and platform prefab created

---
+++ Assets/Scripts/WorldCode/PlatformLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformLogic : MonoBehaviour
+{
+
+    private GameObject player_;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player_ = collision.gameObject;
+            player_.transform.parent = GetComponentInChildren<Transform>().transform;
+        }
+
+        //Debug.Log("He entrado aqui");
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player_.transform.parent = null;
+        }
+    }
+}
+
