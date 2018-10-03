e4ccd819df8863a9708d7d848fc472f4d81a0b80
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2018-10-03 14:25:16 +0200

    Created some new folders in project. Use _Personal folder for your workspace and testing. Set up a test scene for myself and started working on tutorial triggers.

---
+++ Assets/_Personal/Jon/Jon_Scripts/Trigger_Tutorial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigger_Tutorial : MonoBehaviour {
+
+    public bool isPaused;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
