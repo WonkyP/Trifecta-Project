8e16308731f5d66b2979c6fc6d55f12746c1f812
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2018-10-05 12:52:41 +0200

    Updated the finish trigger with ok button and color change for sprites.

--- Assets/Scripts/Trigger_PuzzleRoomFinish.cs
+++ Assets/Scripts/Trigger_PuzzleRoomFinish.cs
@@ -7,9 +7,10 @@ public class Trigger_PuzzleRoomFinish : MonoBehaviour {
 
     public GameObject ScorePresenter;
     public Sprite SoulShard;
-    public Image Score1;
-    public Image Score2;
-    public Image Score3;
+    private Image Score1;
+    private Image Score2;
+    private Image Score3;
+    private Color tmp;
     public Text scoreToDisplay;
 
 	// Use this for initialization
@@ -29,15 +30,21 @@ public class Trigger_PuzzleRoomFinish : MonoBehaviour {
         {
             ScorePresenter.SetActive(true);
             StartCoroutine("ShowScore");
-            
+            ScorePresenter.transform.GetChild(6).gameObject.GetComponent<Button>().onClick.AddListener(OKButton);
         }
     }
 
     IEnumerator ShowScore()
     {
+        tmp.a = 255f;
+        tmp.r = 255f;
+        tmp.g = 255f;
+        tmp.b = 255f;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
         if(GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= 1)
         {
-            Score1.sprite = SoulShard;
+            Score1.sprite = SoulShard;       
+            Score1.color = tmp;
             Debug.Log("Soul sprite attached to score 1!");
             scoreToDisplay.text = "1";
         }
@@ -45,15 +52,23 @@ public class Trigger_PuzzleRoomFinish : MonoBehaviour {
         if (GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= 3)
         {
             Score2.sprite = SoulShard;
+            Score2.color = tmp;
             scoreToDisplay.text = "2";
         }
         yield return new WaitForSeconds(1);
         if (GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= 5)
         {
             Score3.sprite = SoulShard;
+            Score3.color = tmp;
             scoreToDisplay.text = "3";
         }
+
         yield return null;
+    }
+
+    public void OKButton()
+    {
+        ScorePresenter.SetActive(false);
     }
 
     // Update is called once per frame
