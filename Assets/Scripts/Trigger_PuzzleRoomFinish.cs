dba34fa780b62596ae3c7d2bb59d271fd6604f4d
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2018-10-06 14:42:58 +0200

    Integrated puzzleRoom score with an exit interaction.

--- Assets/Scripts/Trigger_PuzzleRoomFinish.cs
+++ Assets/Scripts/Trigger_PuzzleRoomFinish.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Trigger_PuzzleRoomFinish : MonoBehaviour {
+
+    public bool PlayerInRange;
+    public bool InteractionButtonPressed;
+
+    public string SceneToLoad;
 
     public GameObject ScorePresenter;
     public Sprite SoulShard;
@@ -13,35 +19,53 @@ public class Trigger_PuzzleRoomFinish : MonoBehaviour {
     private Color tmp;
     public Text scoreToDisplay;
 
+    [Header("Charges for Score")]
+    [TextArea]
+    public string myTextArea = "Number of charges left to determine score for this challenge";
+    public int ChargesForScore1;
+    public int ChargesForScore2;
+    public int ChargesForScore3;
+
 	// Use this for initialization
 	void Start () {
-
+        //Finding references
         ScorePresenter = GameObject.FindGameObjectWithTag("Canvas_PuzzleRoom").transform.GetChild(2).gameObject;
         SoulShard = Resources.Load<Sprite>("Placeholders/ShardPNG");
         scoreToDisplay = ScorePresenter.transform.GetChild(5).gameObject.GetComponent<Text>();
         Score1 = ScorePresenter.transform.GetChild(1).GetComponent<Image>();
         Score2 = ScorePresenter.transform.GetChild(2).GetComponent<Image>();
         Score3 = ScorePresenter.transform.GetChild(3).GetComponent<Image>();
+        PlayerInRange = false;
+        InteractionButtonPressed = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            ScorePresenter.SetActive(true);
-            StartCoroutine("ShowScore");
-            ScorePresenter.transform.GetChild(6).gameObject.GetComponent<Button>().onClick.AddListener(OKButton);
+            PlayerInRange = true;           
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            PlayerInRange = false;
         }
     }
 
+    //Coroutine for summarazing player performance in puzzle-room
     IEnumerator ShowScore()
     {
+        //Getting color values
         tmp.a = 255f;
         tmp.r = 255f;
         tmp.g = 255f;
         tmp.b = 255f;
-        gameObject.GetComponent<BoxCollider2D>().enabled = false;
-        if(GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= 1)
+        //Disable trigger collider, so it only happens once
+        //gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        //These if statements look at Carl's script to find the current number of soul charges left and gives scores accordingly.
+        if(GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= ChargesForScore1)
         {
             Score1.sprite = SoulShard;       
             Score1.color = tmp;
@@ -49,14 +73,14 @@ public class Trigger_PuzzleRoomFinish : MonoBehaviour {
             scoreToDisplay.text = "1";
         }
         yield return new WaitForSeconds(1);
-        if (GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= 3)
+        if (GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= ChargesForScore2)
         {
             Score2.sprite = SoulShard;
             Score2.color = tmp;
             scoreToDisplay.text = "2";
         }
         yield return new WaitForSeconds(1);
-        if (GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= 5)
+        if (GameObject.Find("Canvas_PuzzleRoom").gameObject.GetComponent<ChardCounter>().curShardCount >= ChargesForScore3)
         {
             Score3.sprite = SoulShard;
             Score3.color = tmp;
@@ -69,10 +93,25 @@ public class Trigger_PuzzleRoomFinish : MonoBehaviour {
     public void OKButton()
     {
         ScorePresenter.SetActive(false);
+        SceneManager.LoadScene(SceneToLoad);
     }
 
     // Update is called once per frame
     void Update () {
-		
+		if(PlayerInRange == true && InteractionButtonPressed == false)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                InteractionButtonPressed = true;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<GeneralPlayerMovement>().enabled = false;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<DaughterMovement>().enabled = false;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<SpiritMovement>().enabled = false;
+                GameObject.FindGameObjectWithTag("Player").GetComponent<FatherMovement>().enabled = false;
+                ScorePresenter.SetActive(true);
+                ScorePresenter.transform.GetChild(6).gameObject.GetComponent<Button>().onClick.AddListener(OKButton);
+                StartCoroutine("ShowScore");
+                
+            }
+        }
 	}
 }
