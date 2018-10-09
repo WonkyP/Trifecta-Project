e15f41738e9e2888da2a87746d67bc68040127c5
carl69 <carlmorgenstierne@yahoo.com>
2018-10-09 22:25:16 +0200

    Fixed a collider in the puzzle level 1 and the door there, added a start menu to reset the playerpref and introduse the player to the game

---
+++ Assets/Scripts/StartMenu/StartMenuButtons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuButtons : MonoBehaviour {
+
+    public void GoToOpenWorld()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(1);
+    }
+}
