edb7361ca6f1636fe6c42b41ff1af175cd70cdd8
Pablo <pablma11@ucm.es>
2018-10-24 16:30:38 +0200

    Starting with shooting Enemy AI

---
+++ Assets/Scripts/AI/ShootingEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : MonoBehaviour {
+
+    public Transform FirePoint;
+    public GameObject Bullet;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    // Script with the shoot logic
+    void Shoot()
+    {
+        Instantiate(Bullet, FirePoint.transform);
+    }
+}
