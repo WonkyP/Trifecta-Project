fff9fb0cfd8b874366929e11ee847ec85b97cf33
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2018-10-31 17:31:33 +0100

    Added a system for playing sounds via animation events. Will continue to refine.

---
+++ Assets/_Personal/Jon/Jon_Scripts/Jon_AnimationEventTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jon_AnimationEventTest : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void PrintEvent(string s)
+    {
+        Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
+    }
+    public void PlaySound(AudioClip clip)
+    {
+        AudioSource.PlayClipAtPoint(clip, gameObject.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
