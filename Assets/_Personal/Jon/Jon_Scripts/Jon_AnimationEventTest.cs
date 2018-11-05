c34b1dba060966cd7fb25bd373abf8a8ca1d7d59
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2018-11-05 17:28:10 +0100

    Changed the way audio plays for player slightly. Updated player prefab and and daughter walking animation

--- Assets/_Personal/Jon/Jon_Scripts/Jon_AnimationEventTest.cs
+++ Assets/_Personal/Jon/Jon_Scripts/Jon_AnimationEventTest.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Jon_AnimationEventTest : MonoBehaviour {
 
+    public AudioSource Movement;
+    public AudioClip[] Footsteps;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +16,10 @@ public class Jon_AnimationEventTest : MonoBehaviour {
     {
         Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
     }
-    public void PlaySound(AudioClip clip)
+    public void PlayFoot()
     {
-        AudioSource.PlayClipAtPoint(clip, gameObject.transform.position);
+        Movement.PlayOneShot(Footsteps[1]);
+        //AudioSource.PlayClipAtPoint(clip, gameObject.transform.position);
     }
 
     // Update is called once per frame
