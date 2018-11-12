85964afd5cb73b18b27d3915d1360244d2924379
Pablo <pablma11@ucm.es>
2018-11-12 12:23:27 +0100

    Updates in the P0202 mechanics

---
+++ Assets/Scripts/DeadZone/DeadZone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour {
+
+    ObjectPooler objectPooler;
+
+    GameObject respawnGo;
+    Vector2 respawnPos;
+
+	// Use this for initialization
+	void Start () {
+        objectPooler = ObjectPooler.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        respawnGo = collision.gameObject;
+        objectPooler.killGameObject(respawnGo);
+
+        if (collision.gameObject.tag == "Box")
+        {
+            respawnPos = collision.gameObject.GetComponent<Box>().getStartPosition();
+            objectPooler.spawnFromPool("Boxes", respawnPos, respawnGo.transform.rotation);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Spawner/StartLevelSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevelSpawner : MonoBehaviour {
+
+    public GameObject BoxSpawner1;
+    public GameObject BoxSpawner2;
+
+	// Use this for initialization
+	void Start () {
+        //ObjectPooler.instance.spawnFromPool("Boxes", BoxSpawner1.transform.position, BoxSpawner1.transform.rotation);
+        //ObjectPooler.instance.spawnFromPool("Boxes", BoxSpawner2.transform.position, BoxSpawner2.transform.rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.P))
+            ObjectPooler.instance.spawnFromPool("Boxes", BoxSpawner1.transform.position, BoxSpawner1.transform.rotation);
+    }
+}
