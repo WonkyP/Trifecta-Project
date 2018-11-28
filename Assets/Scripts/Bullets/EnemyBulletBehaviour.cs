4cbe0f8217331934fadfe8b39be30290dd375d6c
Pablo <pablma11@ucm.es>
2018-11-28 11:55:57 +0100

    Bullets code re-organised

---
+++ Assets/Scripts/Bullets/EnemyBulletBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletBehaviour : MonoBehaviour {
+
+    ObjectPooler objectPooler;
+
+	// Use this for initialization
+	void Start () {
+        objectPooler = ObjectPooler.instance;
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Damage logic
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<GeneralPlayerMovement>().touchedByEnemy(transform.localScale.x / Mathf.Abs(transform.localScale.x), 2);
+            collision.gameObject.GetComponent<GeneralPlayerMovement>().Damaged();
+
+            if (collision.gameObject.GetComponent<SpiritNewMovement>().enabled)
+            {
+                collision.gameObject.GetComponent<SpiritNewMovement>().damaged();
+            }
+            else if (collision.gameObject.GetComponent<DaughterMovement>().enabled)
+            {
+                collision.gameObject.GetComponent<DaughterMovement>().damaged();
+            }
+            else if (collision.gameObject.GetComponent<FatherNewMovement>().enabled)
+            {
+                collision.gameObject.GetComponent<FatherNewMovement>().damaged();
+            }
+        }
+
+        // Deactivating the object
+        objectPooler.killGameObject(gameObject);
+    }
+}
