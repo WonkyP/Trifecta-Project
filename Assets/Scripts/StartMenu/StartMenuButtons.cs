b9b90d59510d87cf3e3d8d21b2f4ba1c6a4b38bb
carl69 <carlmorgenstierne@yahoo.com>
2018-12-03 15:05:31 +0100

    startmenu got a game testing button

--- Assets/Scripts/StartMenu/StartMenuButtons.cs
+++ Assets/Scripts/StartMenu/StartMenuButtons.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,6 +16,17 @@ public class StartMenuButtons : MonoBehaviour {
     public void ContinueGame()
     {
         GameObject.FindGameObjectWithTag("DoorNr").GetComponent<DoNotDestroy>().NameOfTheObject = PlayerPrefs.GetString("EntryPoint", "Tutorial"); // set the exit object
+        GetComponent<LoadScene>().LoadLevel(PlayerPrefs.GetString("LastLoadedScene"));
+    }
+
+    public void StartFromLevel02()
+    {
+        PlayerPrefs.DeleteAll();
+        // set the pp
+        PlayerPrefs.SetString("LastLoadedScene", "Level02"); // Set startScene.
+        PlayerPrefs.SetFloat("D01", 1); // unlock WallJump
+
+
         GetComponent<LoadScene>().LoadLevel(PlayerPrefs.GetString("LastLoadedScene"));
 
 
