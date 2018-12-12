3f0e3bb75e9859b84b2d2d87bf51b428d732782d
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2018-12-12 23:02:58 +0100

    Menu canvas update

---
+++ Assets/Scripts/Menu_AnyKeyDisable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_AnyKeyDisable : MonoBehaviour {
+
+	void OnEnable () {
+        //StartCoroutine(Disable());
+	}
+
+    //IEnumerator Disable()
+    //{
+        
+    //    while (true)
+    //    {
+    //        if (Input.anyKeyDown)
+    //        {
+    //            this.gameObject.SetActive(false);
+    //        }
+    //    }
+
+    //}
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
